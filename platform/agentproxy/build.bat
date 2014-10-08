cfb2c61466a75c400b2826222cf582bf335c0d81
prog.chen <prog.chen@gmail.com>
2014-10-08 11:23:57 +0800

    update config

--- platform/agentproxy/build.bat
+++ platform/agentproxy/build.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if exist output rd /q /s output
 if not exist output\log mkdir .\output\log
 if not exist output\conf mkdir .\output\conf
@@ -8,14 +7,3 @@ if not exist output\conf mkdir .\output\conf
 
 copy .\Release\ApServer.exe output\
 copy .\conf output\conf
-=======
-if exist output rd /q /s output
-if not exist output\log mkdir .\output\log
-if not exist output\conf mkdir .\output\conf
-
-"c:/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE/Devenv" "ApServer.vcproj" /rebuild Release /project ApServer
-"c:/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE/Devenv" "ApServer.vcproj" /rebuild Debug /project ApServer
-
-copy .\Release\ApServer.exe output\
-copy .\conf output\conf
->>>>>>> 52366f05890caf1d08efb5cba831089bd4c4b06f
